f019cca127932120dcf047195347c9fd553549cb
Zain Sheraz <zain.sheraz@gmail.com>
2020-04-16 16:37:57 -0400

    Delete slicing.py

--- slicing.py
+++
@@ -1,5 +0,0 @@
-players = ['player1', 'player2', 'player3', 'player4']
-print(players[0:3])
-#prints all from 0 to 3, EXCLUDING 3
-#can also use without index, to start from beginning or till end
-print(players[:3])
