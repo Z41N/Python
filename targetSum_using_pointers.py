01abc999878e250e2dcced93af33f423b96a760d
Zain Sheraz <zain.sheraz@gmail.com>
2020-04-16 16:57:16 -0400

    Create targetSum_using_pointers.py
    Solution 2 using pointers.

---
+++ targetSum_using_pointers.py
@@ -0,0 +1,15 @@
+# Take advantage of sort property
+def twoNumberSum(array, targetSum):
+	array.sort()
+	left = 0
+	# Len - 1 because of the last index on right 
+	right = len(array) - 1
+	while left < right:
+		combinedSum = array[left] + array[right]
+		if combinedSum == targetSum:
+			return [array[left], array[right]]
+		elif combinedSum < targetSum:
+			left += 1
+		elif combinedSum > targetSum:
+			right -= 1
+	return []
