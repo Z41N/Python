c1608ed8b21e16f4ab950c2ff3a2bbe726a3f2cd
Zain Sheraz <zain.sheraz@gmail.com>
2020-04-17 17:47:58 -0400

    Update tictactoe.py
    Added remaining routes for winning

--- tictactoe.py
+++ tictactoe.py
@@ -30,7 +30,7 @@ def display_board():
 # Allowing player X to make turn
 def handle_turn_x():
     global current_player
-    position = input("Enter a position from 1 to 9: ")
+    position = input("Enter a position from 1 to 9:")
     position = int(position) - 1
     board[position] = "X"
     current_player = "X"
@@ -40,18 +40,60 @@ def handle_turn_x():
 # Allowing player O to make turn
 def handle_turn_o():
     global current_player
-    position = input("Enter a position from 1 to 9: ")
+    position = input("Enter a position from 1 to 9:")
     position = int(position) - 1
     board[position] = "O"
     current_player = "O"
     display_board()
 
 
-# Checking for win
+# Checking for win using row and column checkers
 def win_checker():
-    global is_game_on
+    if row_win_checker():
+        return True
+    elif col_win_checker():
+        return True
+    else:
+        return False
+
+
+# Checking if any rows are winners
+def row_win_checker():
     if board[0] == board[1] == board[2] != "-":
+        print("------")
         print(board[0] + " won!")
+        print("------")
+        return True
+    elif board[3] == board[4] == board[5] != "-":
+        print("------")
+        print(board[3] + " won!")
+        print("------")
+        return True
+    elif board[6] == board[7] == board[8] != "-":
+        print("------")
+        print(board[6] + " won!")
+        print("------")
+        return True
+    else:
+        return False
+
+
+# Checking if any columns are winners
+def col_win_checker():
+    if board[0] == board[3] == board[6] != "-":
+        print("------")
+        print(board[0] + " won!")
+        print("------")
+        return True
+    elif board[1] == board[4] == board[7] != "-":
+        print("------")
+        print(board[1] + " won!")
+        print("------")
+        return True
+    elif board[2] == board[5] == board[8] != "-":
+        print("------")
+        print(board[2] + " won!")
+        print("------")
         return True
     else:
         return False
@@ -66,6 +108,10 @@ def tie_checker():
 def start_game():
     global is_game_on
     is_game_on = True
+    print("\n")
+    print("-----------")
+    print("TIC-TAC-TOE")
+    print("-----------\n")
     display_board()
     while is_game_on:
         handle_turn_x()
