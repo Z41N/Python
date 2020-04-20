a52552051610abc06bcd011b54590e931b1fe9b7
Zain Sheraz <zain.sheraz@gmail.com>
2020-04-20 18:27:45 -0400

    Create RFM.py

---
+++ RFM.py
@@ -0,0 +1,93 @@
+import pandas as pd
+from datetime import timedelta
+import matplotlib.pyplot as plt
+import seaborn as sns
+import squarify
+
+# Reading the csv file
+online = pd.read_csv('/Users/zain/Downloads/data.csv', encoding = "ISO-8859-1")
+
+# Converting InvoiceDate to an actual date format
+online['InvoiceDate'] = pd.to_datetime(online['InvoiceDate'])
+
+# Show number of rows and columns
+print('{:,} rows; {:,} columns'
+      .format(online.shape[0], online.shape[1]))
+
+# Transactions without customer ID
+print('{:,} transactions don\'t have a customer id'
+      .format(online[online.CustomerID.isnull()].shape[0]))
+
+# Number of transactions between 2 given dates
+print('Transactions timeframe from {} to {}'.format(online['InvoiceDate'].min(),
+                                    online['InvoiceDate'].max()))
+
+# Dropping N/A's
+online.dropna()
+
+# Create TotalSum column for online dataset
+online['TotalSum'] = online['Quantity'] * online['UnitPrice']
+
+# Create snapshot date
+snapshot_date = online['InvoiceDate'].max() + timedelta(days=1)
+print(snapshot_date)
+
+# Grouping by CustomerID
+data_process = online.groupby(['CustomerID']).agg({
+        'InvoiceDate': lambda x: (snapshot_date - x.max()).days,
+        'InvoiceNo': 'count',
+        'TotalSum': 'sum'})
+
+# Rename the columns
+data_process.rename(columns={'InvoiceDate': 'Recency',
+                         'InvoiceNo': 'Frequency',
+                         'TotalSum': 'MonetaryValue'}, inplace=True)
+
+# Print top 5 rows and shape of dataframe
+print(data_process.head())
+print('{:,} rows; {:,} columns'
+      .format(data_process.shape[0], data_process.shape[1]))
+
+# Plot RFM distributions
+plt.figure(figsize=(12,10))
+
+# Plot distribution of R
+plt.subplot(3, 1, 1); sns.distplot(data_process['Recency'])
+
+# Plot distribution of F
+plt.subplot(3, 1, 2); sns.distplot(data_process['Frequency'])
+
+# Plot distribution of M
+plt.subplot(3, 1, 3); sns.distplot(data_process['MonetaryValue'])
+
+# Show the plot
+plt.show()
+
+# --Calculate R and F groups--
+# Create labels for Recency and Frequency
+r_labels = range(4, 0, -1); f_labels = range(1, 5)
+
+# Assign these labels to 4 equal percentile groups
+r_groups = pd.qcut(data_process['Recency'], q=4, labels=r_labels)
+
+# Assign these labels to 4 equal percentile groups
+f_groups = pd.qcut(data_process['Frequency'], q=4, labels=f_labels)
+
+# Create new columns R and F
+data_process = data_process.assign(R = r_groups.values, F = f_groups.values)
+data_process.head()
+
+# Create labels for MonetaryValue
+m_labels = range(1, 5)
+
+# Assign these labels to three equal percentile groups
+m_groups = pd.qcut(data_process['MonetaryValue'], q=4, labels=m_labels)
+
+# Create new column M
+data_process = data_process.assign(M = m_groups.values)
+
+# Concat RFM quartile values to create RFM Segments
+def join_rfm(x): return str(x['R']) + str(x['F']) + str(x['M'])
+data_process['RFM_Segment_Concat'] = data_process.apply(join_rfm, axis=1)
+rfm = data_process
+rfm.head()
